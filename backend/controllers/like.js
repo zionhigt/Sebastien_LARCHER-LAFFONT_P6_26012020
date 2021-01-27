c6d9e8d6ca19c73d2dd15a4adbde709cf7109557
ZionHigt <larcher501@gmail.com>
2021-01-27 17:04:17 +0100

    Init

--- backend/controllers/like.js
+++ backend/controllers/like.js
@@ -3,18 +3,22 @@ const Sauce = require('../models/Sauce');
 
 module.exports.likeHandler = (req, res, next) =>
 {
+	// On cherche l'utilisateur en BDD
 	User.findOne({_id: req.body.userId})
 	.then(user => {
+		// On cherche la sauce en BDD
 		Sauce.findOne({_id: req.params.id})
 		.then(sauce => {
+			// Selon la valeur de body.like
 			switch(req.body.like)
 			{
 				case -1:
+					// si userId n'est pas dans la tableau de dislike  
 					if(sauce.usersDisliked.indexOf(user.id) == -1)
 					{
-						sauce.usersDisliked.push(user.id)
+						sauce.usersDisliked.push(user.id)// On ajoute l'userId au tableau
 						const isInto = sauce.usersLiked.indexOf(user.id);
-						if(isInto > -1)
+						if(isInto > -1)// Si l'userId est trouvé dans le tableau des like on le supprime
 						{
 							sauce.usersLiked.splice(isInto, 1)
 						}
@@ -27,24 +31,26 @@ module.exports.likeHandler = (req, res, next) =>
 
 					const isIntoLike = sauce.usersLiked.indexOf(user.id);
 					const isIntoDislike = sauce.usersDisliked.indexOf(user.id);
-					if(isIntoLike > -1)
+					if(isIntoLike > -1)// Si l'userId est trouvé dans le tableau des like on le supprime
 					{
-						sauce.usersLiked.splice(isInto, 1);
+						sauce.usersLiked.splice(isIntoLike, 1);
 
 					}
-					else if(isIntoDislike > -1)
+
+					if(isIntoDislike > -1)// Si l'userId est trouvé dans le tableau de dislke on le supprime
 					{
-						sauce.usersDisliked.splice(isInto, 1);
+						sauce.usersDisliked.splice(isIntoDislike, 1);
 
 					}
+
 					break;
 
 				case 1:
-					if(sauce.usersLiked.indexOf(user.id) == -1)
+					if(sauce.usersLiked.indexOf(user.id) == -1)// Si l'userId n'est pas trouvé dans le tableau des likes on l'ajoute
 					{
 						sauce.usersLiked.push(user.id)
 						const isInto = sauce.usersDisliked.indexOf(user.id);
-						if(isInto > -1)
+						if(isInto > -1)// Si l'userId est trouvé dans le tableau des dislikes on le supprime
 						{
 							sauce.usersDisliked.splice(isInto, 1)
 						}
@@ -54,19 +60,21 @@ module.exports.likeHandler = (req, res, next) =>
 					break;
 
 				default:
+					// Si body.like n'a auccune de ces trois valeurs, null est renvoyer a l'utilisateur
 					res.status(200).json({message: null});
 					break;
 
 			}
 
-			sauce.likes = sauce.usersLiked.length
-			sauce.dislikes = sauce.usersDisliked.length
+			sauce.likes = sauce.usersLiked.length;
+			sauce.dislikes = sauce.usersDisliked.length;
+			// les quantités de like/dislike sont mis à jour
 			sauce.save(sauce)
 			.then((sauce) => {
-				console.log(sauce);
 				res.status(200).json({message: "Done !"})
 			})
 			.catch(error => {res.status(500).json({ error })});
+			// Les likes/dislikes de la sauce sont mis à jour
 		})
 		.catch(error => {res.status(404).json({ error })});
 
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const app = require('./app');
 
-
+// retourne un port en nombre entier et en base decimal
 const normalizePort = val => {
 	const port = parseInt(val, 10);
 	if(isNaN(port))
@@ -16,9 +16,10 @@ const normalizePort = val => {
 	return false;
 };
 
-const port = normalizePort(process.env.PORT || '3000');
-app.set('port', port);
+const port = normalizePort(process.env.PORT || '3000');//Definie le port selon une variable d'environnement ou definit le port 3000
+app.set('port', port);//Defini le port d'ecoute du serveur
 
+////////////////////////Gestionaire d'erreurs/////////////////////////
 const errorHandler = error => {
 	if(error.syscall !== "listen")
 	{
@@ -41,13 +42,14 @@ const errorHandler = error => {
 			throw error;
 	}
 };
-const server = http.createServer(app);
+const server = http.createServer(app);//initialise un serveur http
 
-server.on('error', errorHandler);
+server.on('error', errorHandler);// Callback en cas d'erreurs
 server.on('listening', ()=>{
 	const address = server.address();
 	const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
 	console.log('Listening on' + bind);
 })
 
+// le serveur ecoute sur le port 'port'
 server.listen(port);
